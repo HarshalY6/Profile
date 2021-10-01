ab356d0fbeeb03fc742882f11de537ca6119d518
HarshalY6 <harshya75@gmail.com>
2021-10-01 17:22:55 +0530

    Sample Profile Page

---
+++ lib/logic.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Save {
+  void write(String name, String abt, String no) {
+    print(name);
+    print(abt);
+    print(no);
+  }
+}
+
+class Inimage {
+  var picker;
+  var ima = File('lib\assets\bumblebee.jpg');
+  im() async {
+    picker = new ImagePicker();
+    final getFile = await picker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 50,
+        preferredCameraDevice: CameraDevice.front);
+    print('Done');
+    return getFile;
+  }
+
+  // input() {
+  //   return ima;
+  // }
+}
---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'logic.dart';
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Profile',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String no = '';
+  String abt = '';
+  String name = '';
+  final s = Save();
+
+  final i = Inimage();
+
+  var ima;
+  var getFile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+            backgroundColor: Colors.grey[850],
+            title: Text(
+              'Profile',
+              style: TextStyle(color: Colors.white),
+            )),
+        body: SingleChildScrollView(
+            child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Stack(
+                children: <Widget>[
+                  Center(
+                      child: ClipOval(
+                          child: ima != null
+                              ? Image.file(ima,
+                                  width: 200.0, height: 200.0, fit: BoxFit.fill)
+                              : Container(
+                                  decoration:
+                                      BoxDecoration(color: Colors.red[200]),
+                                  width: 200,
+                                  height: 200,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.grey[800],
+                                  ),
+                                ))),
+                  Container(
+                    padding: const EdgeInsets.all(5.0),
+                    alignment: Alignment.bottomRight,
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.add_a_photo_rounded,
+                        color: Colors.red,
+                        size: 30.0,
+                      ),
+                      onPressed: () async {
+                        getFile = await i.im();
+                        setState(() {
+                          ima = File(getFile.path);
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 330,
+                child: TextField(
+                    onSubmitted: (value) => name = value,
+                    style: TextStyle(
+                        fontSize: 21, height: 0.8, color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: 'Name',
+                      labelStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                      border: InputBorder.none,
+                    ))),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 330,
+                child: TextField(
+                    // keyboardType: TextInputType.multiline,
+                    // minLines: 1,
+                    // maxLines: 5,
+                    onSubmitted: (value) => abt = value,
+                    style: TextStyle(
+                        fontSize: 21, height: 0.8, color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: 'About',
+                      labelStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                      border: InputBorder.none,
+                    ))),
+            SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+                width: 330,
+                child: TextField(
+                    onSubmitted: (value) => no = value,
+                    style: TextStyle(
+                        fontSize: 21, height: 0.8, color: Colors.white),
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      labelStyle: TextStyle(color: Colors.grey, fontSize: 20),
+                      border: InputBorder.none,
+                    ))),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                s.write(name, abt, no);
+              },
+              child: const Text('Save'),
+            ),
+          ]),
+        )));
+  }
+}
